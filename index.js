7cc728cf24d64ed253e5ed6bc35bcb2d4c55d711
Mayank Dungarwal <73541497+mayankd07@users.noreply.github.com>
2021-08-01 22:30:55 +0530

    changes port number

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'home.html'));
   });
   
-  
+const port = 6000 ;
   app.listen(port, () => {
     console.log("Server running at port : " + port);
   });
